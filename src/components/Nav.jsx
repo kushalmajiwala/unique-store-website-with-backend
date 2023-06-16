a54a6d477073ab2724de0a6c2d34c27ea4306b08
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-06-16 18:07:43 +0530

    navbar small changes of hover

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,22 +24,22 @@ const Nav = () => {
             <div className=''>
                 <ul className='hidden md:flex justify-between items-center pr-4'>
                     <li className='p-2 w-32 text-center px-4 mt-2 hover:bg-gray-200 rounded-3xl'>
-                        <NavLink to='/' className='no-underline text-black'>HOME</NavLink>
+                        <NavLink to='/' className='no-underline text-gray-500 opacity-100'>HOME</NavLink>
                     </li>
                     <li className='p-2 w-32 text-center px-4 mt-2 hover:bg-gray-200 rounded-3xl'>
-                        <NavLink to='/about' className='no-underline text-black'>ABOUT</NavLink>
+                        <NavLink to='/about' className='no-underline text-gray-500 opacity-100'>ABOUT</NavLink>
                     </li>
                     <li className='p-2 w-32 text-center px-4 mt-2 hover:bg-gray-200 rounded-3xl'>
-                        <NavLink to='/products' className='no-underline text-black'>PRODUCTS</NavLink>
+                        <NavLink to='/products' className='no-underline text-gray-500 opacity-100'>PRODUCTS</NavLink>
                     </li>
                     <li className='p-2 w-32 text-center px-4 mt-2 hover:bg-gray-200 rounded-3xl'>
-                        <NavLink to='/contact' className='no-underline text-black'>CONTACT</NavLink>
+                        <NavLink to='/contact' className='no-underline text-gray-500 opacity-100'>CONTACT</NavLink>
                     </li>
                     <li className='p-2 w-32 text-center px-4 mt-2'>
                         <NavLink to='/contact' className='no-underline text-white bg-blue-500 rounded-md py-2 px-4'>LOGIN</NavLink>
                     </li>
                     <li className='p-2 w-32 text-center px-4 mt-2'>
-                        <i className="bi bi-cart p-overlay-badge text-3xl cursor-pointer">
+                        <i className="bi bi-cart p-overlay-badge text-3xl cursor-pointer text-gray-500 opacity-100 hover:text-black">
                             <Badge value="0" severity="danger"></Badge>
                         </i>
                     </li>
@@ -54,22 +54,22 @@ const Nav = () => {
                     <div className='inline-block md:flex text-center'>
                         <ul className={`${navVisibility} md:hidden pr-4 text-center text-2xl w-screen h-screen`}>
                             <li className='p-3 mt-2'>
-                                <NavLink to='/' className='no-underline text-black' onClick={updateNavVisibility}>HOME</NavLink>
+                                <NavLink to='/' className='no-underline text-gray-500 opacity-100' onClick={updateNavVisibility}>HOME</NavLink>
                             </li>
                             <li className='p-3 mt-2'>
-                                <NavLink to='/about' className='no-underline text-black' onClick={updateNavVisibility}>ABOUT</NavLink>
+                                <NavLink to='/about' className='no-underline text-gray-500 opacity-100' onClick={updateNavVisibility}>ABOUT</NavLink>
                             </li>
                             <li className='p-3 mt-2'>
-                                <NavLink to='/products' className='no-underline text-black' onClick={updateNavVisibility}>PRODUCTS</NavLink>
+                                <NavLink to='/products' className='no-underline text-gray-500 opacity-100' onClick={updateNavVisibility}>PRODUCTS</NavLink>
                             </li>
                             <li className='p-3 mt-2'>
-                                <NavLink to='/contact' className='no-underline text-black' onClick={updateNavVisibility}>CONTACT</NavLink>
+                                <NavLink to='/contact' className='no-underline text-gray-500 opacity-100' onClick={updateNavVisibility}>CONTACT</NavLink>
                             </li>
                             <li className='p-3 mt-2'>
                                 <NavLink to='/contact' className='no-underline text-white bg-blue-500 rounded-md py-2 px-4'  onClick={updateNavVisibility}>LOGIN</NavLink>
                             </li>
                             <li className='p-3 mt-2'>
-                                <i className="bi bi-cart p-overlay-badge text-3xl cursor-pointer" onClick={updateNavVisibility}>
+                                <i className="bi bi-cart p-overlay-badge text-3xl cursor-pointer text-gray-500 opacity-100" onClick={updateNavVisibility}>
                                     <Badge value="0" severity="danger"></Badge>
                                 </i>
                             </li>
