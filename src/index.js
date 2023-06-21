480fe3363781f2aa629e511a3ab3df80a8b2b817
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-06-21 18:48:01 +0530

    contextAPI, useReducer, AXIOS Done

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-                                    
+import { AppProvider } from './context/productcontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AppProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppProvider>
 );
