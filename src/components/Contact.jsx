9fef075afc4a6663920107370abc374ceff29723
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-06-23 17:49:15 +0530

    contact page completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Contact = () => {
     return (
         <>
-            <div>Contact</div>
+            <div className='flex justify-center py-10'>
+                <div className='p-3 w-full'>
+                    <h2 className='text-center mb-5'>Feel Free to Contact Us</h2>
+                    {/* eslint-disable-next-line */}
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.606900655041!2d72.79964237445552!3d21.207769380486766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04c287031a507%3A0x7883a27798cc9de4!2sVirat%20Shakti%20Apartment!5e0!3m2!1sen!2sin!4v1687520301088!5m2!1sen!2sin" width="100%" height="470" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+            <div className='flex justify-center py-10'>
+                <div className=' w-5/6 md:w-2/6'>
+                    <form action='https://formspree.io/f/mjvqbvvl' method='POST'>
+                        <input type='text' placeholder='Username' name='Username' required autoComplete='off' className=' border-1 border-blue-300 w-full h-8 pl-2'/><br/><br/>
+                        <input type='email' placeholder='Email' name='Email' autoComplete='off' required  className=' border-1 border-blue-300 w-full h-8 pl-2'/><br/><br/>
+                        <textarea name='Message' cols="30" rows="10" required autoComplete="off" placeholder="Enter your Message" className=' border-1 border-blue-300 w-full pl-2'></textarea><br/><br/>
+                        <input type="submit" value="SEND" className='w-36 h-10 text-white text-lg bg-blue-500 rounded-xl hover:bg-blue-600' />
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import FormatPrice from '../helpers/FormatPrice';
 
 const Product = (curElem) => {
 
@@ -11,12 +12,12 @@ const Product = (curElem) => {
                 <div className='bg-white pt-3 px-3 pb-1 md:w-5/6 shadow-md mt-4 hover:scale-110'>
                     <figure className='relative'>
                         <img src={image} alt={name} className='h-44 md:h-40 md:w-72'/>
-                        <figcaption className='absolute bg-white top-2 right-2 text-gray-700 rounded-xl w-fit px-3 py-1 text-sm'>{category}</figcaption>
+                        <figcaption className='absolute bg-white top-2 right-2 text-gray-500 rounded-xl w-fit px-3 py-1 text-sm'>{category}</figcaption>
                     </figure>
                     <div>
                         <div className='flex justify-between items-center'>
                             <p className='text-lg'>{name}</p>
-                            <p className=' text-sm text-gray-400'>{price}</p>
+                            <p className=' text-sm text-gray-400'><FormatPrice price={price} /></p>
                         </div>
                     </div>
                 </div>
